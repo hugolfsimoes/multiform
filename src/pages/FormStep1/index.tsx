e8ff7b3478352d8f206a3da1e4ef6f3fa1a7e5fc
hugolfsimoes <hugolfsimoes@gmail.com>
2022-01-27 19:26:47 -0300

    feito o step 1 e step 2

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,10 +1,34 @@
-import { Theme } from '../../components/Theme';
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../context/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    });
+  }, []);
 
   const handleNextStep = () => {
+    if (state.name) {
+      navigate('./step2');
+    } else {
+      alert("Preencha os dados");
+    }
 
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    });
   };
 
   return (
@@ -21,6 +45,8 @@ export const FormStep1 = () => {
           <input
             type="text"
             autoFocus
+            value={ state.name }
+            onChange={ handleNameChange }
           />
         </label>
         <button onClick={ handleNextStep }>Próximo</button>
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,9 +1,63 @@
+import { useNavigate, Link } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../context/FormContext';
+import { Theme } from '../../components/Theme';
+import { useEffect } from 'react';
+import { SelectOptions } from '../../components/SelectOptions';
 
 export const FormStep2 = () => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === '') {
+      navigate('.././');
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 2
+      });
+    }
+  }, []);
+
+  const handleNextStep = () => {
+    navigate('.././step3');
+
+  };
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level
+    });
+  };
   return (
-    <C.Container>
-      Passo 2
-    </C.Container>
+    <Theme >
+      <C.Container>
+        <p>Passo 2/3</p>
+        <h1>{ state.name }, o que melhor escreve você?</h1>
+        <p>Escolha a opção que melhor condiz com seu estado atual, profissionalmente.</p>
+
+        <hr />
+
+        <SelectOptions
+          title="Sou iniciante"
+          description="Comecei a programar há menos de 2 anos"
+          icon="🥳"
+          selected={ state.level === 0 }
+          onClick={ () => setLevel(0) }
+        />
+
+        <SelectOptions
+          title="Sou programador"
+          description="Já programo a 2 anos ou mais"
+          icon="🤓"
+          selected={ state.level === 1 }
+          onClick={ () => setLevel(1) }
+
+        />
+        <Link to="/" className='backButton'>Voltar</Link>
+        <button onClick={ handleNextStep }>Próximo</button>
+      </C.Container>
+    </Theme>
   );
 };
